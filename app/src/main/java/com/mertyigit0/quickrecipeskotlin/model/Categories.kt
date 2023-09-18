030191ae5d32e9050847e742d01ba2550a9ecfc5
MertY <mertyigit148@gmail.com>
2023-09-18 11:50:16 +0300

    ViewBinding and data class Categories

---
+++ app/src/main/java/com/mertyigit0/quickrecipeskotlin/model/Categories.kt
@@ -0,0 +1,12 @@
+package com.mertyigit0.quickrecipeskotlin.model
+
+data class Categories(
+    val idCategory : Int? ,
+    val strCategory : String? ,
+    val strCategoryThumb: String? ,
+    val strCategoryDescription: String?
+
+
+
+
+)
