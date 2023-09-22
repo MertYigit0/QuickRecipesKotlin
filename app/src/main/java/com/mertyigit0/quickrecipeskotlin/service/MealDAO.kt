96027f6cb7719aaa799f0bbee4a6631846faefef
MertY <mertyigit148@gmail.com>
2023-09-22 17:53:41 +0300

    internet bug

--- app/src/main/java/com/mertyigit0/quickrecipeskotlin/service/MealDAO.kt
+++ app/src/main/java/com/mertyigit0/quickrecipeskotlin/service/MealDAO.kt
@@ -10,17 +10,16 @@ import com.mertyigit0.quickrecipeskotlin.model.MealModel
 @Dao
 interface MealDAO {
     @Insert
-    suspend fun insertMeal(meal: MealModel)
-
-    @Update
-    suspend fun updateMeal(meal: MealModel)
-
-    @Delete
-    suspend fun deleteMeal(meal: MealModel)
+    suspend fun insertALL(vararg meals: MealModel):List<Long>
 
     @Query("SELECT * FROM meals")
     suspend fun getAllMeals(): List<MealModel>
 
     @Query("SELECT * FROM meals WHERE idMeal = :mealId")
-    suspend fun getMealById(mealId: String): MealModel?
+    suspend fun getMealById(mealId: String): MealModel
+
+
+    @Query("DELETE FROM meals")
+    suspend fun deleteAllMeal()
+
 }
--- app/src/main/java/com/mertyigit0/quickrecipeskotlin/viewmodel/InsideCategoryViewModel.kt
+++ app/src/main/java/com/mertyigit0/quickrecipeskotlin/viewmodel/InsideCategoryViewModel.kt
@@ -1,16 +1,20 @@
 package com.mertyigit0.quickrecipeskotlin.viewmodel
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mertyigit0.quickrecipeskotlin.model.CategoriesModel
 import com.mertyigit0.quickrecipeskotlin.model.MealModel
+import com.mertyigit0.quickrecipeskotlin.service.APIDatabase
 import com.mertyigit0.quickrecipeskotlin.service.MealApiService
 import com.mertyigit0.quickrecipeskotlin.util.CustomSharedPreferences
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
 class InsideCategoryViewModel(application: Application) : BaseViewModel(application) {
 
@@ -21,15 +25,36 @@ class InsideCategoryViewModel(application: Application) : BaseViewModel(applicat
     private  val mealApiService = MealApiService()
     private  val disposable = CompositeDisposable()
 
-
+    private  var customSharedPreferences = CustomSharedPreferences(getApplication())
+    private  var refreshTime = 10_000L * 60 * 1_000_000_000L
 
 
 
     fun refreshData(){
 
-        getDataFromAPI()
+        val updateTime = customSharedPreferences.getTime()
+        if (updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refreshTime) {
+            getDataFromSQLite()
+
+        } else {
+            getDataFromAPI()
+        }
 
     }
+
+    @SuppressLint("SuspiciousIndentation")
+    private  fun getDataFromSQLite(){
+        launch {
+
+            val meals = APIDatabase(getApplication()).mealDao().getAllMeals()
+            showMeals(meals)
+
+            Toast.makeText(getApplication(),"SQLite", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
     fun setSelectedCategory(category: String) {
         selectedCategory = category
     }
@@ -46,8 +71,8 @@ class InsideCategoryViewModel(application: Application) : BaseViewModel(applicat
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<MealModel>>() {
                     override fun onSuccess(t: List<MealModel>) {
-                        meals.value = t
-                        mealError.value = false
+                        storeInSQLite(t)
+                        Toast.makeText(getApplication(),"API",Toast.LENGTH_LONG).show()
                     }
 
                     override fun onError(e: Throwable) {
@@ -57,6 +82,46 @@ class InsideCategoryViewModel(application: Application) : BaseViewModel(applicat
                     }
                 })
         )
+    }
+
+
+private fun showMeals(mealList : List<MealModel>){
+    meals.value = mealList
+    mealError.value = false
+
+}
+
+
+
+    private fun storeInSQLite(list : List<MealModel>){
+    launch {
+        val dao = APIDatabase(getApplication()).mealDao()
+        dao.deleteAllMeal()
+        val instertIds = dao.insertALL(*list.toTypedArray())
+        var i = 0
+        while (i<list.size){
+            list[i].uuid = instertIds[i].toInt()
+            i = i +1
+        }
+        showMeals(list)
+    }
+        customSharedPreferences.saveTime(System.nanoTime())
+
+    }
+
+
+
+
+
+
+
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+
+        disposable.clear()
     }
 
 
