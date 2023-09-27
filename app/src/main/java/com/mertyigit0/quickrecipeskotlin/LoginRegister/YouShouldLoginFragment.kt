c8a9a93b6aa68cc9d4fbcc8b5732b6df192945a1
MertY <mertyigit148@gmail.com>
2023-09-27 14:38:37 +0300

    You should Login Fragment

---
+++ app/src/main/java/com/mertyigit0/quickrecipeskotlin/LoginRegister/YouShouldLoginFragment.kt
@@ -0,0 +1,50 @@
+package com.mertyigit0.quickrecipeskotlin.LoginRegister
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.mertyigit0.quickrecipeskotlin.R
+import com.mertyigit0.quickrecipeskotlin.databinding.FragmentRegisterBinding
+import com.mertyigit0.quickrecipeskotlin.databinding.FragmentYouShouldLoginBinding
+
+
+class YouShouldLoginFragment : Fragment() {
+    private var _binding: FragmentYouShouldLoginBinding? = null;
+    private val binding get() = _binding!!;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding =   FragmentYouShouldLoginBinding.inflate(inflater,container,false)
+        val view = binding.root;
+        return view
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(requireView())
+
+
+
+        binding.loginNowButton.setOnClickListener{
+
+            navController.navigate(R.id.action_youShouldLoginFragment_to_loginFragment2)
+
+
+        }
+
+
+
+
+    }
+
+}
